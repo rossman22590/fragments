c1514297bf26e58e9f661581ed8b8fbad1424de0
Ross Cohen <rosscohen1990@gmail.com>
2024-10-07 02:28:05 -0400

    u

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Fragments by E2B',
-  description: "Open-source version of Anthropic's Artifacts",
+  title: 'AIT Code Builder',
+  description: "Super Powered Artifacts",
 }
 
 export default function RootLayout({
